dab5b5b70878ef48bbabdec733b41fdb0da9cc81
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2016-09-29 18:01:57 +0200

    Extract error radii

--- extract_nw.py
+++ extract_nw.py
@@ -13,8 +13,8 @@ rblock = False
 wblock = False
 allroots = []
 allweights = []
-roots = []
-weights = []
+allrootserr = []
+allweightserr = []
 
 with open(f, "r") as F:
     for line in F.readlines():
@@ -22,13 +22,17 @@ with open(f, "r") as F:
             rblock = True
             wblock = False
             roots = []
+            rootserr = []
             allroots.append(roots)
+            allrootserr.append(rootserr)
             continue
         elif line.startswith("The weights are"):
             wblock = True
             rblock = False
             weights = []
+            weightserr = []
             allweights.append(weights)
+            allweightserr.append(weightserr)
             continue
         elif (rblock or wblock) and not line.startswith("| "):
             rblock = False
@@ -37,15 +41,19 @@ with open(f, "r") as F:
         elif rblock:
             N = [float(s.replace(' ', '')) for s in re.findall("[-+]?[ ]?\d+\.?\d*[Ee]?[+-]?\d*", line)]
             roots.append(N[0] + 1.0j * N[1])
+            rootserr.append(N[2] + 1.0j * N[3])
         elif wblock:
             W = [float(s.replace(' ', '')) for s in re.findall("[-+]?[ ]?\d+\.?\d*[Ee]?[+-]?\d*", line)]
             weights.append(W[0] + 1.0j * W[1])
+            weightserr.append(W[2] + 1.0j * W[3])
 
 if not allroots or not allweights:
     raise ValueError("No suitable data found!")
 
 allroots = map(array, allroots)
 allweights = map(array, allweights)
+allrootserr = map(array, allrootserr)
+allweightserr = map(array, allweightserr)
 
 b = os.path.basename(f).rsplit(".", 1)[0]
 
@@ -57,6 +65,9 @@ for i, w in enumerate(allweights):
     with open(b + "_weights_" + str(i) + ".dat", "w") as f:
         savez(f, w)
 
-for i, (r, w) in enumerate(zip(allroots, allweights)):
+for i, (r, w, rerr, werr) in enumerate(zip(allroots, allweights, allrootserr, allweightserr)):
     with open(b + "_qr_" + str(i) + ".csv", "w") as f:
-        savetxt(f, array([real(r), imag(r), real(w), imag(w)]), delimiter=",")
+        savetxt(f, array([real(r), imag(r),
+                          real(w), imag(w),
+                          real(rerr), imag(rerr),
+                          real(werr), imag(werr)]), delimiter=", ")
