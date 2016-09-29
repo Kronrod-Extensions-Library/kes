61df9b2372814b5358f5796510a2e45d5fd48196
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2016-09-29 17:26:59 +0200

    Finish parallel rule computing script

--- rulecomputer.py
+++ rulecomputer.py
@@ -1,11 +1,8 @@
 import sys
-import os.path
 import concurrent.futures
 import subprocess
 
-from rulelistfile import collect_all_rules, get_rulelist
-
-
+from rulelistfile import get_rulelist
 
 
 def compute_rules(rulelist, call, max_workers=8):
@@ -21,29 +18,25 @@ def compute_rules(rulelist, call, max_workers=8):
                 def doit():
                     ruledatafile = "rule_{}.txt".format("_".join(map(str, rule)))
                     with open(ruledatafile, 'a') as stdout:
-                        subprocess.call(call(rule),
-                                        shell=False,
-                                        stdout=stdout)
+                        subprocess.call(call(rule), shell=False, stdout=stdout)
 
                 return doit
 
             F.append(executor.submit(compute_rule(rule)))
 
-
         for future in concurrent.futures.as_completed(F):
             future.result()
-
 
 
 if __name__ == '__main__':
 
     call = lambda rule: ['./kes', '-cn', '-cw', *map(str, rule)]
 
-    if not len(sys.argv[1:]) > 1:
+    if not len(sys.argv[1:]) >= 1:
         raise IOError('No input files given to read rules from.')
 
-    # TODO: Merge all rules before calling computep
-    for listfile in sys.argv[1:]:
-        print(listfile)
-        rules = get_rulelist(listfile)
-        compute_rules(rules, call)
+    # Merge all rules before calling computep
+    rules = [rule for listfile in sys.argv[1:] for rule in get_rulelist(listfile)]
+
+    # Compute all rules in parallel
+    compute_rules(rules, call)
